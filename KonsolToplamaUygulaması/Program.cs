b578ac6ea4e2138e4ab4dbd65fdbe4a4f6c425e9
Arif Dere <123989496+arif42deree@users.noreply.github.com>
2024-09-07 21:44:14 +0300

    Add files via upload

---
+++ KonsolToplamaUygulaması/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsolToplamaUygulaması
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            String sayi1;
+            String sayi2;
+            int donusum1;
+            int donusum2;
+            int toplam;
+            int sonuc;
+            Console.WriteLine("birinci Sayıyı giriniz:");
+            sayi1 = Console.ReadLine();
+            Console.WriteLine("ikinci sayıyı giriniz:");
+            sayi2 = Console.ReadLine();
+            donusum1 = Convert.ToInt32(sayi1);
+            donusum2 = Convert.ToInt32(sayi2);
+            toplam = donusum1 + donusum2;
+            sonuc = toplam;
+            Console.WriteLine("girdiğiniz iki sayının toplamı: " + sonuc);
+            Console.ReadKey();
+
+
+        }
+    }
+}
