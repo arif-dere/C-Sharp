630f74bd2518ab2a52e5a10a828ab86ca94ba5f5
Arif Dere <123989496+arif-dere@users.noreply.github.com>
2024-09-16 21:59:54 +0300

    Add files via upload

---
+++ kelimeCeviriBuyukKucuk/kelimeCeviriBuyukKucuk/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace kelimeCeviriBuyukKucuk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Çevirmek istediğiniz türü seçin (büyük/küçük)");
+                string turSecim = Console.ReadLine();
+                if (turSecim == "büyük" || turSecim == "buyuk")
+                {
+                    Console.Write("Çevrilecek kelimeyi veya cümleyi giriniz: ");
+                    string cevirBuyuk = (Console.ReadLine());
+                    Console.WriteLine("Sonuç: {0}", cevirBuyuk.ToUpper());
+                }
+                else if (turSecim == "küçük" || turSecim == "kucuk")
+                {
+                    Console.Write("Çevrilecek kelimeyi veya cümleyi giriniz: ");
+                    string cevirKucuk = Console.ReadLine();
+                    Console.WriteLine("Sonuç: {0}", cevirKucuk.ToLower());
+                }
+                else
+                {
+                    Console.WriteLine("Hata! İki Seçenekten herhangi birisini Seçmediniz...");
+                }
+            }
+        }
+    }
+}
